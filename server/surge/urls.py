f7636b8d63f0986e8b2031e4c99d0fb0c17fb8a4
abdulfataiaka <abdulfataiaka@gmail.com>
2020-03-17 12:52:31 +0100

    chage file structure

--- server/surge/urls.py
+++ server/surge/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from surge.views import RootView
+from surge.views import root_view
 from django.urls import path, include
+from surge.views import page_not_found_view, server_error_view
 
+
+handler500 = server_error_view
+handler404 = page_not_found_view
 
 urlpatterns = [
-    path('', RootView.as_view(), name='root'),
-    path('ratings/', include(('ratings.urls', 'ratings'))),
+    path('', root_view, name='root'),
     path('admin/', admin.site.urls),
+    path('ratings/', include(('ratings.urls', 'ratings'))),
 ]
--- server/surge/views.py
+++ server/surge/views.py
@@ -1,8 +1,13 @@
-from django.views import View
 from django.urls import reverse
+from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
 
-class RootView(View):
-    def get(self, request):
-        return HttpResponseRedirect(reverse('ratings:index'))
+def root_view(request):
+    return HttpResponseRedirect(reverse('ratings:index'))
+
+def server_error_view(request, exception=None):
+    return render(request, 'surge/500.html', status=500)
+
+def page_not_found_view(request, exception=None):
+    return render(request, 'surge/404.html', status=404)
