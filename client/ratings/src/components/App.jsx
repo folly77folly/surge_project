a744de204c725069e543b771c1db6529b64ea0c7
Bond <lovedammy77@yahoo.com>
2020-03-18 15:27:09 +0100

    Convert client to CRA

--- client/ratings/src/components/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const App = () => (
-    <h1 style={{textAlign: 'center', marginTop: '6em'}}>
-        Surge: Decagon Internal Tool
-    </h1>
-);
-
-export default App;
